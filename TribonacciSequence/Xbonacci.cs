b35d6cda410dddf458cff896bf27c7b841da5ac9
Shiva Ramdeen <shiva.ramdeen@teleios-systems.com>
2019-01-29 13:37:13 -0400

    started Tribonacci Class

---
+++ TribonacciSequence/Xbonacci.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TribonacciSequence
+{
+    public class Xbonacci
+    {
+        public double[] Tribonacci(double[] signature, int n)
+        {
+            List<double> result = new List<double>();
+            if(n == 0)
+            {
+                return result.ToArray();
+            }
+
+            Parallel.For(0, n, i =>
+            {
+
+            });
+        }
+    }
+}
