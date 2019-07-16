100ba41d53715c1be5e944af08dd24e4eff3cb17
Shiva Ramdeen <shiva.ramdeen@outlook.com>
2019-07-16 16:01:31 -0400

    attempted DisenvowelTrolls https://www.codewars.com/kata/disemvowel-trolls/csharp

---
+++ DisemvowelTrolls/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+//Link to problem
+//https://www.codewars.com/kata/disemvowel-trolls/csharp
+namespace DisemvowelTrolls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static string Disemvowel(string str)
+        {
+            string pattern = "[aeiouAEIOUyY]";
+            string replacement = "";
+
+            Regex regEx = new Regex(pattern);
+            string trolled = regEx.Replace(str, replacement);
+            return trolled;
+        }
+    }
+}
